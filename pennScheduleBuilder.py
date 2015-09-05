8ac686c89238bf6518a09d53e1f022bda2e26362
Alec Olesky <aolesky@gmail.com>
2015-09-05 16:35:09 -0400

    Made slight updates

--- pennScheduleBuilder.py
+++ pennScheduleBuilder.py
@@ -6,31 +6,45 @@ registrar = Registrar('UPENN_OD_emFc_1001364', '6kl4eonkquheuti65e32qick6l')
 def set_requirements(reqs):
 	if reqs === None or len(reqs) === 0:
 		return 'Must have at least one requirement'
+
+	total_credits = 0
+	for req in reqs:
+		total_credits = total_credits + reqs[req]
 	
-	## ADD REAL ERROR HANDLING
+	if total_credits > 6.5:
+		return 'Why the fuck are you taking more than six and a half classes, go outside'
 	
 
-def find_all_classes(reqType, course_level_above=None, course_level_below=None, starts_after=None, starts_on_day=None):
+def find_all_classes(reqType, course_level_above=None, course_level_below=None, starts_after=None):
+	# First enter default params for all courses
 	search_params = {}
 	search_params['term'] = '2015C'
-	search_params[]
-	if course_level_above is not None:
-		search_params['course_level_above'] = course_level_above
-	if course_level_below is not None:
-		search_params['course_level_below'] = course_level_below
+	search_params['status'] = 'O'
+	search_params['is_cancelled'] = False
+	# if course_level_above is not None:
+	# 	search_params['course_level_above'] = course_level_above
+	# if course_level_below is not None:
+	# 	search_params['course_level_below'] = course_level_below
 	if starts_after is not None:
 		search_params['starts_at_or_after_hour'] = starts_after
-	if starts_on_day is not None:
-		search_params['starts_on_day'] = starts_on_day
 
 	all_courses = registrar(search_params)
 
-def enter_requirements(req_numbers, otherParams):
-	self.req_types = {}
+def enter_requirements(req_numbers, starts_after=None, ends_before=None, no_class_days=None, difficulty_limit=None, work_limit=None, lunch_break=None):
+	self.req_classes = {}
 	
 	for req in req_numbers:
-		self.req_types[req] = self.findAllClasses(req, params) # FIX PARAM STUFF HERE
+		self.req_classes[req] = self.findAllClasses(req, starts_after=starts_after) # FIX PARAM STUFF HERE
+		self.filter_class_days(self.req_classes, no_class_days)
 
+
+
+def filter_class_days(req_classes, no_class_days):
+	for req in req_classes:
+		current_classes = req_classes[req]
+		for i in range(0, len(req_classes[req]) - 1):
+			if self.class_meets_on_days(current_classes[i]):
+				del current_classes[i]
 
 
 def find_schedule(req_course_list, req_numbers, selected_classes):
@@ -73,4 +87,3 @@ def find_schedule(req_course_list, req_numbers, selected_classes):
 				return selected_classes
 
 	return False
-
