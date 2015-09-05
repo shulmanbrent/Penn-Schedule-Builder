150109fb9af87e586d9d0dda7f75d2351dc4a11e
sponcebus <lakesp@seas.upenn.edu>
2015-09-05 17:42:26 -0400

    updated to fit parser

--- courseComparator.py
+++ courseComparator.py
@@ -1,33 +1,54 @@
 import jsonCourseParser as parser
 
-def courses_are_same(first_course, second_course):
-	first_course_title = first_course["course_department"] 
-		+ first_course["course_number"]
-	second_course_title = second_course["course_department"] 
-		+ second_course["course_number"]
-	return first_course_title == second_course_title
-
-
-def courses_share_meeting_days(first_course, second_course):
-	first_course_meeting_days = list(first_course["meeting_days"])
-	second_course_meeting_days = list(second_course["meeting_days"])
-	for meeting_day in first_course_meeting_days:
-		if meeting_day in second_course_meeting_days:
+def course_meets_on_days(course, days_to_check):
+	course_meeting_days = list(course["meetings_days"])
+	for day_to_check in days_to_check:
+		if day_to_check in course_meeting_days:
 			return true
 	return false
 
+def courses_are_same(first_course, second_course):
+	first_course_title = parser.get_course_department(first_course) 
+		+ parser.get_course_number(first_course)
+	second_course_title = parser.get_course_department(second_course) 
+		+ parser.get_course_number(second_course)
+	return first_course_title == second_course_title
 
-def course_meeting_times_overlap(first_course, second_course):
-	first_course_start_time = first_course["start_time_24"]
-	first_course_end_time = first_course["end_time_24"]
-	second_course_start_time = second_course["start_time_24"]
-	second_course_end_time = second_course["end_time_24"]
-	if first_course_start_time > second_course_start_time 
-		and first_course_start_time < second_course_start_time:
+
+def shared_meeting_days(first_course, second_course):
+	first_course_meeting_days = parser.get_meetings(first_course).keys()
+	second_course_meeting_days = parser.get_meetings(second_course).keys()
+	days_overlapped = list()
+	for meeting_day in first_course_meeting_days:
+		if meeting_day in second_course_meeting_days:
+			days_overlapped.append(meeting_day)
+	return days_overlapped
+
+
+def course_times_overlap(first_course_time, second_course_time):
+	first_course_start = first_course_time[0]
+	first_course_end = first_course_time[1]
+	second_course_start = second_course_time[0]
+	second_course_end = second_course_time[1]
+	if first_course_start > second_course_start and 
+		first_course_start < second_course_end:
 		return true
-	if second_course_start_time > first_course_start_time 
-		and second_course_start_time < first_course_start_time:
+	if second_course_start > first_course_start and 
+		second_course_start < first_course_end:
 		return true
+	return false
+
+
+def course_meeting_times_overlap(first_course, second_course, days_overlapped):
+	first_course_meetings = parser.get_meetings(first_course)
+	second_course_meetings = parser.get_meetings(second_course)
+	for day in days_overlapped:
+		first_course_times = first_course_meetings[day]
+		second_course_times = second_course_meetings[day]
+		for first_course_time in first_course_times:
+			for second_course_time in second_course_times:
+				if coure_times_overlap(first_course_time, second_course_time):
+					return true
 	return false
 
 
@@ -35,8 +56,10 @@ def courses_overlap(first_course, second_course):
 	if courses_are_same(first_course, second_course):
 		return true
 	else:
-		return courses_share_meeting_days(first_course, second_course) and
-			course_meeting_times_overlap(first_course, second_course)
+		shared_days = shared_meeting_days(first_course, second_course)
+		if shared_days.len() == 0:
+			return false
+		return course_meeting_times_overlap(first_course, second_course, shared_days)
 
 
 def filter_out_courses_that_overlap(first_course, courses_to_compare):
@@ -64,13 +87,6 @@ def filter_out_courses_that_overlap(first_course, courses_to_compare):
 #		courses_that_fit = filter_out_courses_that_overlap(least_difficult_course, courses_that_fit)
 #	return schedule
 		
-
-def course_meets_on_days(course, days_to_check):
-	course_meeting_days = list(course["meetings_days"])
-	for day_to_check in days_to_check:
-		if day_to_check in course_meeting_days:
-			return true
-	return false
 
 
 
