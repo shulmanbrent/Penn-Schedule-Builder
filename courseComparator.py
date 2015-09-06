699f52691ba35e53f5ca6d097fd86bdf86bb7854
sponcebus <lakesp@seas.upenn.edu>
2015-09-05 22:49:29 -0400

    added tests

--- courseComparator.py
+++ courseComparator.py
@@ -1,11 +1,11 @@
 import jsonCourseParser as parser
 
-def course_meets_on_days(course, days_to_check):
-	course_meeting_days = list(course["meetings_days"])
-	for day_to_check in days_to_check:
-		if day_to_check in course_meeting_days:
-			return True
-	return False
+# def course_meets_on_days(course, days_to_check):
+# 	course_meeting_days = list(course["meetings_days"])
+# 	for day_to_check in days_to_check:
+# 		if day_to_check in course_meeting_days:
+# 			return True
+# 	return False
 
 def courses_are_same(first_course, second_course):
 	first_course_title = parser.get_course_department(first_course) \
@@ -62,30 +62,14 @@ def courses_overlap(first_course, second_course):
 		return course_meeting_times_overlap(first_course, second_course, shared_days)
 
 
-#def filter_out_courses_that_overlap(first_course, courses_to_compare):
-# 	courses_that_fit = list()
-# 	for course_to_compare in courses_to_compare:
-# 		if not courses_overlap(first_course, course_to_compare):
-# 			courses_that_fit.append(course_to_compare)
-# 	return courses_that_fit
+def filter_out_courses_that_overlap(first_course, courses_to_compare):
+	courses_that_fit = list()
+	for course_to_compare in courses_to_compare:
+		if not courses_overlap(first_course, course_to_compare):
+			courses_that_fit.append(course_to_compare)
+	return courses_that_fit
 
 
-# def get_least_difficult_course(all_courses):
-# 	least_diffcult = all_courses[0]
-# 	for course in all_courses:
-# 		if course["course_difficulty"] < least_diffcult["course_difficulty"]:
-# 			least_diffcult = course
-# 	return least_diffcult
-
-
-# def pick_best_schedule(all_courses, number_of_courses_to_take):
-# 	schedule = list()
-# 	courses_that_fit = all_courses
-# 	while (schedule.len() < number_of_courses_to_take and courses_that_fit.len() > 0):
-# 		least_difficult_course = get_least_difficult_course(courses_that_fit)
-# 		schedule.append(least_difficult_course)
-# 		courses_that_fit = filter_out_courses_that_overlap(least_difficult_course, courses_that_fit)
-# 	return schedule
 		
 
 
